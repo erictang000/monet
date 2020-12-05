1b7fa43dadb9b888f4d69dee1671d841996ca6b0
Eric Tang <erictang000@gmail.com>
2020-12-04 16:28:32 -0800

    training with many dense layers

--- src/classification_net_training.py
+++ src/classification_net_training.py
@@ -1,7 +1,7 @@
 import numpy as np
 from keras.models import Model
 from keras.layers import Dense,BatchNormalization,Conv1D
-from keras.layers import Input,GlobalMaxPooling1D,concatenate
+from keras.layers import Input,GlobalMaxPooling1D, GlobalAveragePooling1D, concatenate
 from keras.optimizers import Adam
 from utils import generate
 import tensorflow as tf
@@ -14,7 +14,7 @@ history = History()
 
 batchsize = 32
 T = np.arange(19,21,0.1) # this provides another layer of stochasticity to make the network more robust
-steps = 52
+steps = 38
  # number of steps to generate
 initializer = 'he_normal'
 f = 32 #number of filters
@@ -28,7 +28,7 @@ x1 = Conv1D(f,2,dilation_rate=2,padding='causal',activation='relu',kernel_initia
 x1 = BatchNormalization()(x1)
 x1 = Conv1D(f,2,dilation_rate=4,padding='causal',activation='relu',kernel_initializer=initializer)(x1)
 x1 = BatchNormalization()(x1)
-x1 = GlobalMaxPooling1D()(x1)
+x1 = GlobalAveragePooling1D()(x1)
 
 
 x2 = Conv1D(f,2,padding='causal',activation='relu',kernel_initializer=initializer)(inputs)
@@ -37,7 +37,7 @@ x2 = Conv1D(f,2,dilation_rate=3,padding='causal',activation='relu',kernel_initia
 x2 = BatchNormalization()(x2)
 x2 = Conv1D(f,2,dilation_rate=5,padding='causal',activation='relu',kernel_initializer=initializer)(x2)
 x2 = BatchNormalization()(x2)
-x2 = GlobalMaxPooling1D()(x2)
+x2 = GlobalAveragePooling1D()(x2)
 
 
 x3 = Conv1D(f,3,padding='causal',activation='relu',kernel_initializer=initializer)(inputs)
@@ -46,7 +46,7 @@ x3 = Conv1D(f,3,dilation_rate=2,padding='causal',activation='relu',kernel_initia
 x3 = BatchNormalization()(x3)
 x3 = Conv1D(f,3,dilation_rate=4,padding='causal',activation='relu',kernel_initializer=initializer)(x3)
 x3 = BatchNormalization()(x3)
-x3 = GlobalMaxPooling1D()(x3)
+x3 = GlobalAveragePooling1D()(x3)
 
 
 x4 = Conv1D(f,3,padding='causal',activation='relu',kernel_initializer=initializer)(inputs)
@@ -55,7 +55,7 @@ x4 = Conv1D(f,3,dilation_rate=3,padding='causal',activation='relu',kernel_initia
 x4 = BatchNormalization()(x4)
 x4 = Conv1D(f,3,dilation_rate=5,padding='causal',activation='relu',kernel_initializer=initializer)(x4)
 x4 = BatchNormalization()(x4)
-x4 = GlobalMaxPooling1D()(x4)
+x4 = GlobalAveragePooling1D()(x4)
 
 
 x5 = Conv1D(f,4,padding='causal',activation='relu',kernel_initializer=initializer)(inputs)
@@ -64,7 +64,7 @@ x5 = Conv1D(f,4,dilation_rate=2,padding='causal',activation='relu',kernel_initia
 x5 = BatchNormalization()(x5)
 x5 = Conv1D(f,4,dilation_rate=4,padding='causal',activation='relu',kernel_initializer=initializer)(x5)
 x5 = BatchNormalization()(x5)
-x5 = GlobalMaxPooling1D()(x5)
+x5 = GlobalAveragePooling1D()(x5)
 
 x7 = Conv1D(f,4,padding='causal',activation='relu',kernel_initializer=initializer)(inputs)
 x7 = BatchNormalization()(x7)
@@ -72,17 +72,20 @@ x7 = Conv1D(f,4,dilation_rate=3,padding='causal',activation='relu',kernel_initia
 x7 = BatchNormalization()(x7)
 x7 = Conv1D(f,4,dilation_rate=5,padding='causal',activation='relu',kernel_initializer=initializer)(x7)
 x7 = BatchNormalization()(x7)
-x7 = GlobalMaxPooling1D()(x7)
+x7 = GlobalAveragePooling1D()(x7)
 
 
 x6 = Conv1D(f,3,padding='same',activation='relu',kernel_initializer=initializer)(inputs)
 x6 = BatchNormalization()(x6)
-x6 = GlobalMaxPooling1D()(x6)
+x6 = GlobalAveragePooling1D()(x6)
 
 
 con = concatenate([x1,x2,x3,x4,x5,x6])
 dense = Dense(512,activation='relu')(con)
+dense = Dense(256,activation='relu')(dense)
 dense = Dense(128,activation='relu')(dense)
+dense = Dense(64,activation='relu')(dense)
+dense = Dense(32,activation='relu')(dense)
 dense2 = Dense(3,activation='softmax')(dense)
 model = Model(inputs=inputs, outputs=dense2)
 
